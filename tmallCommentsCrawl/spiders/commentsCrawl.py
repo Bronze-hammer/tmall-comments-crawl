98e6c3a28c92010c9970cb8bb22c89d88294541f
Bronze-hammer <zihui_xu@126.com>
2020-11-03 00:16:31 +0800

    简单测试，没通过！！！

--- tmallCommentsCrawl/spiders/commentsCrawl.py
+++ tmallCommentsCrawl/spiders/commentsCrawl.py
@@ -4,7 +4,10 @@ import scrapy
 class CommentscrawlSpider(scrapy.Spider):
     name = 'commentsCrawl'
     allowed_domains = ['tmall.com']
-    start_urls = ['http://tmall.com/']
+    start_urls = ['https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15493768257.69.36cb30e8TaJe63&id=625746717520&rn=29c9c4da824d6c3934e736fb08e3ba59&abbucket=20']
 
     def parse(self, response):
-        pass
+        bodys = response.css('div.rate-grid > table > tbody')
+        for body in bodys:
+            print(body.css('div.tm-rate-fulltxt::text').extract_first().strip())
+            break
