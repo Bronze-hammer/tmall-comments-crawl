28a4047d2c862f4128240e642e464614afb7e52e
Bronze-hammer <zihui_xu@126.com>
2020-11-03 20:58:27 +0800

    修饰了请求，但是已经没有爬取成功，可能ip被天猫禁了

--- tmallCommentsCrawl/spiders/commentsCrawl.py
+++ tmallCommentsCrawl/spiders/commentsCrawl.py
@@ -3,11 +3,14 @@ import scrapy
 
 class CommentscrawlSpider(scrapy.Spider):
     name = 'commentsCrawl'
-    allowed_domains = ['tmall.com']
-    start_urls = ['https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15493768257.69.36cb30e8TaJe63&id=625746717520&rn=29c9c4da824d6c3934e736fb08e3ba59&abbucket=20']
+    allowed_domains = ['detail.tmall.com']
+    start_urls = [
+        'https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w5001-21647916242.5.611830e8CALfIx&id=617954262029&rn=7ce83012541598e5a3a6b406eeb18248&abbucket=1&skuId=4478575424436&scene=taobao_shop'
+    ]
 
     def parse(self, response):
-        bodys = response.css('div.rate-grid > table > tbody')
+        bodys = response.css('div.rate-grid > table > tbody');
         for body in bodys:
-            print(body.css('div.tm-rate-fulltxt::text').extract_first().strip())
-            break
+            yield {
+                'comment' : body.css('div.tm-rate-fulltxt::text')
+            }
